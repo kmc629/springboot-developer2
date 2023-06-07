b617a06b305d16d9eccabbbefd43548decaec6a3
kmc629 <kmcboss7@gmail.com>
2023-06-07 23:28:03 +0900

    Initial commit

---
+++ src/main/java/me/kimmincheon/springbootdeveloper/repository/BlogRepository.java
@@ -0,0 +1,7 @@
+package me.kimmincheon.springbootdeveloper.repository;
+
+import me.kimmincheon.springbootdeveloper.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Article, Long> {
+}
